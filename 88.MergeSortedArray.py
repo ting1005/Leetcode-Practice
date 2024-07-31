808c925572938cc3b9583199db30b4c9d1b69530
Ting Chen <wtchen9786@gmail.com>
2024-07-31 19:55:01 +0800

    feat(Array/String): Add new file.

---
+++ 88.MergeSortedArray.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1[m:] = nums2
+        nums1.sort()
+        
