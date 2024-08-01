c0cc0d1c5c7e1b0ad7a1d15143f04f7157404605
Ting Chen <wtchen9786@gmail.com>
2024-08-01 23:21:16 +0800

    feat(Array): Add 88 comment.

--- Array&String/88.MergeSortedArray.py
+++ Array&String/88.MergeSortedArray.py
@@ -2,6 +2,9 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # nums1 = nums1[:m] + nums2
         nums1[m:] = nums2
+        # .sort() is in-place sorting -> return None
+        # sorted() is not in-place sorting -> return a new list
         nums1.sort()
         
